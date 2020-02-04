539bde91bfc6054713064b91cbf9ea199c748f4e
omesser <odedmesser@gmail.com>
2020-02-05 01:03:10 +0200

    Exp1

--- local_v3io_frames/http.py
+++ local_v3io_frames/http.py
@@ -59,43 +59,18 @@ class Client(ClientBase):
         self._session = None
 
         # establish for the first time
-        self._reestablish_session()
+        self._establish_session()
 
     def __del__(self):
         self._session.close()
 
-    def _reestablish_session(self):
-        if self._session is not None:
-            self._session.close()
-
+    def _establish_session(self):
         self._session = requests.sessions.Session()
         self._session.verify = False
 
-    def _safe_execute_request(self, action, *args, **kwargs):
-        """
-        This wrapper safely retries to teardown and re-establish the session upon NewConnectionError
-            This happens when the client get's instantiated and used very rapidly and tcp connections aren't
-            being closed in time by the request package. Instead of using connection:close on all requests or
-            killing tcp keepalives, we handle this error like so, so a healthy persistent use of the client will
-            be able to enjoy healthy session reuse between requests
-
-        :param action: post, get, update, put ... session request operations
-        :param args: list args to be passed to session request
-        :param kwargs: kwargs to be passed session request
-        :return:
-        """
-        retries = 3
-        for retry in range(retries):
-            try:
-                return getattr(self._session, action)(*args, **kwargs)
-            except NewConnectionError as exc:
-                print(f'retrying {retry}')
-                if retry < retries - 1:
-                    self._reestablish_session()
-                    continue
-                raise exc
-        print(f'WTF {retries}')
-        raise RuntimeError('Crap')
+        # we disable keep alive on the session
+        if not self._persist_connection:
+            self._session.keep_alive = False
 
     def _fix_address(self, address):
         if '://' not in address:
@@ -125,7 +100,7 @@ class Client(ClientBase):
         request.update(kw)
 
         url = self._url_for('read')
-        resp = self._safe_execute_request('post', url, json=request, headers=self._headers(json=True), stream=True)
+        resp = self._session.post(url, json=request, headers=self._headers(json=True), stream=True)
         if not resp.ok:
             raise Error('cannot call API - {}'.format(resp.text))
 
@@ -147,7 +122,7 @@ class Client(ClientBase):
         frames = (enc(df2msg(df, labels, index_cols)) for df in dfs)
         data = chain([request], frames)
 
-        resp = self._safe_execute_request('post', url, headers=headers, data=data)
+        resp = self._session.post(url, headers=headers, data=data)
 
         if not resp.ok:
             raise Error('cannot call API - {}'.format(resp.text))
@@ -167,7 +142,7 @@ class Client(ClientBase):
 
         url = self._url_for('create')
         headers = self._headers()
-        resp = self._safe_execute_request('post', url, headers=headers, json=request)
+        resp = self._session.post(url, headers=headers, json=request)
         if not resp.ok:
             raise CreateError(resp.text)
 
@@ -188,7 +163,7 @@ class Client(ClientBase):
         url = self._url_for('delete')
         headers = self._headers()
         # TODO: Make it DELETE ?
-        resp = self._safe_execute_request('post', url, headers=headers, json=request)
+        resp = self._session.post(url, headers=headers, json=request)
         if not resp.ok:
             raise CreateError(resp.text)
 
@@ -205,7 +180,7 @@ class Client(ClientBase):
 
         url = self._url_for('exec')
         headers = self._headers()
-        resp = self._safe_execute_request('post', url, headers=headers, json=request)
+        resp = self._session.post(url, headers=headers, json=request)
         if not resp.ok:
             raise ExecuteError(resp.text)
 
