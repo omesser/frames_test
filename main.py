d3c377968aacbcc76509f507a0fbe15bfce28d3b
omesser <odedmesser@gmail.com>
2020-02-04 23:43:53 +0200

    Testing http again

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def handler(context, event):
     df = pd.DataFrame(data={'cpu': 123}, index=[datetime.now()])
 
     # client = v3f.Client("https://framesd.default-tenant.app.dev65.lab.iguazeng.com", container="users",token="3452431e-30a8-42eb-b381-afc0dc3f579b")
-    # client = v3f.Client("http://framesd:8080", container="users", token="3452431e-30a8-42eb-b381-afc0dc3f579b")
-    client = v3f.Client("grpc://framesd:8081", container="users", token="3452431e-30a8-42eb-b381-afc0dc3f579b")
+    client = v3f.Client("http://framesd:8080", container="users", token="3452431e-30a8-42eb-b381-afc0dc3f579b")
+    # client = v3f.Client("grpc://framesd:8081", container="users", token="3452431e-30a8-42eb-b381-afc0dc3f579b")
     client.write('tsdb', 'avi', df, labels={'something': context.worker_id})
     return ""
