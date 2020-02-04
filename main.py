ce7faeb019cb5551d2aa87a6f1f36a32a23516c5
omesser <odedmesser@gmail.com>
2020-02-05 01:13:41 +0200

    Persist check

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 def init_context(context):
     # client = v3f.Client("https://framesd.default-tenant.app.dev65.lab.iguazeng.com", container="users",token="3452431e-30a8-42eb-b381-afc0dc3f579b")
     client = v3f.Client("http://framesd:8080", container="users", token="3452431e-30a8-42eb-b381-afc0dc3f579b", persist_connection=True)
-    # client = v3f.Client("grpc://framesd:8081", container="users", token="3452431e-30a8-42eb-b381-afc0dc3f579b")
+    # client = v3f.Client("grpc://framesd:8081", container="users", token="3452431e-30a8-42eb-b381-afc0dc3f579b", persist_connection=True)
     setattr(context.user_data, 'frames_client', client)
 
 
@@ -17,7 +17,7 @@ def handler_good(context, event):
     df = pd.DataFrame(data={'cpu': 123}, index=[datetime.now()])
 
     # reusing client
-    context.user_data.client.write('tsdb', 'avi', df, labels={'something': context.worker_id})
+    context.user_data.frames_client.write('tsdb', 'avi', df, labels={'something': context.worker_id})
     return ""
 
 
