da50208c5d602128f294be18fc82f2630eeec2b0
omesser <odedmesser@gmail.com>
2020-02-05 04:59:10 +0200

    timeout ms

--- local_v3io_frames/grpc.py
+++ local_v3io_frames/grpc.py
@@ -63,9 +63,9 @@ class Client(ClientBase):
 
         if not self._persist_connection:
             self._channel_options.extend([
-                # ('grpc.keepalive_time_ms', 10000),
+                ('grpc.keepalive_time_ms', 0),
                 # ('grpc.keepalive_timeout_ms', 5000),
-                ('grpc.keepalive_permit_without_calls', False),
+                # ('grpc.keepalive_permit_without_calls', False),
                 # ('grpc.http2.max_pings_without_data', 0),
                 # ('grpc.http2.min_time_between_pings_ms', 10000),
                 # ('grpc.http2.min_ping_interval_without_data_ms', 5000),
