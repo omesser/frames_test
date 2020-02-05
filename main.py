d7fb95ab04c23185570c170903ac1d8465ead914
omesser <odedmesser@gmail.com>
2020-02-05 02:23:51 +0200

    ugh

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from datetime import datetime
 def init_context(context):
     context.logger.warn('v3io_framsed version: ' + v3f.__version__)
     context.logger.warn('requests version: ' + requests.__version__)
-    context.logger.warn('grpc version: ' + + grpc.__version__)
+    context.logger.warn('grpc version: ' + grpc.__version__)
 
     # client = v3f.Client("https://framesd.default-tenant.app.dev65.lab.iguazeng.com", container="users",token="3452431e-30a8-42eb-b381-afc0dc3f579b")
     # client = v3f.Client("http://framesd:8080", container="users", token="3452431e-30a8-42eb-b381-afc0dc3f579b", persist_connection=True)
